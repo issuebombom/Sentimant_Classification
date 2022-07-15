438f1a5c06061ec705f64c9f23e96e23bd6a34bd
issuebombom <79882498+issuebombom@users.noreply.github.com>
2022-07-15 18:25:34 +0900

    Update params_data.py

--- utils/params_data.py
+++ utils/params_data.py
@@ -2,11 +2,10 @@
 train_path = "dataset/train.csv"
 test_path = "dataset/test.csv"
 
-# Saved model path
-checkpoint_path = '../save/'
+# Sequence length
+SEQ_LEN = 128
 
-SEQ_LEN = 128 # Sequence length
-
+# train hyper-parameter
 validation_split = 0.2
 epochs = 5
 batch_size = 125
